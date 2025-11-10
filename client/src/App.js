80811bc94dada4a8cffb515d4f912568d2222816
ChayaphonKultanon <chayaphon33793@gmail.com>
2025-11-10 18:43:01 +0700

    init

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setChat((prev) => [...prev, data]);
+    });
+    return () => socket.off("receive_message");
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("send_message", message);
+    setMessage("");
+  };
+
+  return (
+    <div className="App" style={{ padding: 20 }}>
+      <h2>Simple Chat</h2>
+      <div>
+        {chat.map((msg, i) => (
+          <p key={i}>{msg}</p>
+        ))}
+      </div>
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Enter message"
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+}
+
+export default App;
